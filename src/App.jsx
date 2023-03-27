e0fa148585faae931544eb8e57cb9b99effc56ab
Sedbastian <SebastianIgnacioLuciani@gmail.com>
2023-03-26 21:29:58 -0300

    Implement upload and show personal photo

--- src/App.jsx
+++ src/App.jsx
@@ -3,36 +3,44 @@ import "./App.css";
 import uniqid from "uniqid";
 import Section from "./components/Section";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBriefcase } from "@fortawesome/free-solid-svg-icons";
+import {
+	faBriefcase,
+	faEnvelope,
+	faPhone,
+	faLocationDot,
+	faCakeCandles,
+	faGlobe
+} from "@fortawesome/free-solid-svg-icons";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
 
 class App extends Component {
 	constructor() {
 		super();
 		this.state = {
-			infoPers: {
-				nombre: "",
-				ocupacion: "",
-				email: "",
-				telefono: "",
-				direccion: "",
-				nacimiento: "",
-				nacionalidad: ""
-			},
-			habilidades: [
-				{ id: uniqid(), nombre: "una", info: "selha" },
-				{ id: uniqid(), nombre: "dosa", info: "polio" },
-				{ id: uniqid(), nombre: "terca", info: "mili" }
-			],
-			idiomas: [{ id: uniqid(), nombre: "iadsdei", info: "blopa" }],
-			experiencia: [{ id: uniqid(), nombre: "daiichi", info: "sohji" }],
 			personal: [
-				{ id: uniqid(), nombre: "nombre", info: "" },
-				{ id: uniqid(), nombre: "ocupacion", info: "" },
-				{ id: uniqid(), nombre: "email", info: "" },
-				{ id: uniqid(), nombre: "telefono", info: "" },
-				{ id: uniqid(), nombre: "direccion", info: "" },
-				{ id: uniqid(), nombre: "nacimiento", info: "" },
-				{ id: uniqid(), nombre: "nacionalidad", info: "" }
+				{ id: uniqid(), field: "Nombre completo", info: "" },
+				{ id: uniqid(), field: "Ocupación", info: "" },
+				{ id: uniqid(), field: "image", info: "" },
+				{ id: uniqid(), field: "Email", info: "" },
+				{ id: uniqid(), field: "Teléfono", info: "" },
+				{ id: uniqid(), field: "Dirección", info: "" },
+				{ id: uniqid(), field: "Fecha de Nacimiento", info: "" },
+				{ id: uniqid(), field: "Nacionalidad", info: "" }
+			],
+			habilidades: [
+				{ id: uniqid(), field: "una", info: "selha" },
+				{ id: uniqid(), field: "dosa", info: "polio" },
+				{ id: uniqid(), field: "terca", info: "mili" }
+			],
+			idiomas: [{ id: uniqid(), field: "iadsdei", info: "blopa" }],
+			experiencia: [
+				{
+					id: uniqid(),
+					field: "daiichi",
+					info: "sohji",
+					from: "2002",
+					to: "2009"
+				}
 			]
 		};
 	}
@@ -48,53 +56,81 @@ class App extends Component {
 		let addObj = {};
 		addObj[stateName] = this.state[stateName].concat({
 			id: uniqid(),
-			nombre: "",
+			field: "",
 			info: ""
 		});
 		this.setState(addObj);
 	};
 
+	iconsPersonal = {
+		email: faEnvelope,
+		telefono: faPhone,
+		direccion: faLocationDot,
+		nacimiento: faCakeCandles,
+		nacionalidad: faGlobe
+	};
+
 	render() {
 		return (
-			<main className="app">
-				<h1><FontAwesomeIcon icon={faBriefcase}/> CV App</h1>
-				<Section
-					stateName="personal"
-					heading="Información Personal"
-					singular="null"
-					editableFields="false"
-					updateMethod={this.updateInfo}
-					addItemMethod="null"
-					sectionState={this.state.personal}
-				/>
-				<Section
-					stateName="habilidades"
-					heading="Habilidades"
-					singular="Habilidad"
-					editableFields="true"
-					updateMethod={this.updateInfo}
-					addItemMethod={this.addItem}
-					sectionState={this.state.habilidades}
-				/>
-				<Section
-					stateName="idiomas"
-					heading="Idiomas"
-					singular="Idioma"
-					editableFields="true"
-					updateMethod={this.updateInfo}
-					addItemMethod={this.addItem}
-					sectionState={this.state.idiomas}
-				/>
-				<Section
-					stateName="experiencia"
-					heading="Experiencia Laboral"
-					singular="Experiencia Laboral"
-					editableFields="true"
-					updateMethod={this.updateInfo}
-					addItemMethod={this.addItem}
-					sectionState={this.state.experiencia}
-				/>
-			</main>
+			<div className="app">
+				<header>
+					<h1>
+						<FontAwesomeIcon icon={faBriefcase} /> CV App
+					</h1>
+				</header>
+				<main>
+					<Section
+						stateName="personal"
+						heading="Información Personal"
+						singular="null"
+						editableFields="false"
+						updateMethod={this.updateInfo}
+						icons={this.iconsPersonal}
+						addItemMethod="null"
+						fromToFields="false"
+						sectionState={this.state.personal}
+					/>
+					<Section
+						stateName="habilidades"
+						heading="Habilidades"
+						singular="Habilidad"
+						editableFields="true"
+						updateMethod={this.updateInfo}
+						icons="null"
+						addItemMethod={this.addItem}
+						fromToFields="false"
+						sectionState={this.state.habilidades}
+					/>
+					<Section
+						stateName="idiomas"
+						heading="Idiomas"
+						singular="Idioma"
+						editableFields="true"
+						updateMethod={this.updateInfo}
+						icons="null"
+						addItemMethod={this.addItem}
+						fromToFields="false"
+						sectionState={this.state.idiomas}
+					/>
+					<Section
+						stateName="experiencia"
+						heading="Experiencia Laboral"
+						singular="Experiencia Laboral"
+						editableFields="true"
+						updateMethod={this.updateInfo}
+						icons="null"
+						addItemMethod={this.addItem}
+						fromToFields="true"
+						sectionState={this.state.experiencia}
+					/>
+				</main>
+				<footer className="footerTag">
+					<a href="https://github.com/Sedbastian">
+						Sedbastian <FontAwesomeIcon icon={faGithub} />
+					</a>
+					&nbsp;2023
+				</footer>
+			</div>
 		);
 	}
 }
--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -2,35 +2,57 @@ import React, { Component } from "react";
 import "./styles/Section.css";
 import uniqid from "uniqid";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-// import {
-
-// } from "@fortawesome/free-solid-svg-icons";
+import {
+	faCheck,
+	faPlus,
+	faPenToSquare
+} from "@fortawesome/free-solid-svg-icons";
+import astronaut from "./user-astronaut-solid.svg";
 
 class Section extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			action: "Save"
+			action: "Save",
+			personalImage: astronaut
 		};
 	}
 
 	onSubmitSection = e => {
-		const { stateName, editableFields, updateMethod } = this.props;
+		const { stateName, editableFields, updateMethod, fromToFields } =
+			this.props;
 		e.preventDefault();
 		let stateUpdate = {};
 		stateUpdate[stateName] = [];
 		e.target.querySelectorAll("form>div").forEach(div => {
-			let nombre;
+			let field;
+			const info = div.children[1].value;
 			if (editableFields === "true") {
-				nombre = div.children[0].value;
+				field = div.children[0].value;
 			} else if (editableFields === "false") {
-				nombre = div.children[0].textContent;
+				field = div.children[0].textContent;
 			}
-			stateUpdate[stateName].push({
-				id: uniqid(),
-				nombre: nombre,
-				info: div.children[1].value
-			});
+			if (fromToFields === "false") {
+				if (field !== "" || info !== "") {
+					stateUpdate[stateName].push({
+						id: uniqid(),
+						field: field,
+						info: info
+					});
+				}
+			} else if (fromToFields === "true") {
+				const from = div.querySelector("#from").value;
+				const to = div.querySelector("#to").value;
+				if (field !== "" || info !== "" || from !== "" || to !== "") {
+					stateUpdate[stateName].push({
+						id: uniqid(),
+						field: field,
+						info: info,
+						from: from,
+						to: to
+					});
+				}
+			}
 		});
 		updateMethod(stateUpdate);
 		this.setState({ action: "Edit" });
@@ -40,13 +62,24 @@ class Section extends Component {
 		this.setState({ action: "Save" });
 	};
 
+	fileUploaded = file => {
+		console.log("fileUploaded", file);
+		this.setState({personalImage: URL.createObjectURL(file)});
+	};
+
+	onPhotoClick = e => {
+		document.getElementById("fileInput").click();
+	};
+
 	render() {
 		const {
 			stateName,
 			heading,
 			singular,
 			editableFields,
+			icons,
 			addItemMethod,
+			fromToFields,
 			sectionState
 		} = this.props;
 
@@ -71,37 +104,87 @@ class Section extends Component {
 							addItemMethod(stateName);
 						}}
 					>
-						Agregar {singular}
+						<FontAwesomeIcon icon={faPlus} className="icon" />
+						<div>Agregar {singular}</div>
 					</button>
 				);
 			}
+			const formClass = `editableFields-${editableFields}`;
 			sectionRender = (
 				<div>
-					<form onSubmit={this.onSubmitSection} autoComplete="on">
+					<form
+						onSubmit={this.onSubmitSection}
+						autoComplete="on"
+						className={formClass}
+					>
 						{sectionState.map((item, index) => {
+							let inputInfo = <input type="text" defaultValue={item.info} />;
 							let inputOrDiv;
-							if (editableFields === "true") {
+							let notEditableClass = null;
+							if (item.field === "image") {
+								notEditableClass = `notEditable${index}`;
 								inputOrDiv = (
 									<input
-										type="text"
-										defaultValue={item.nombre}
+										type="file"
+										id="fileInput"
+										accept="image/*"
+										style={{ display: "none" }}
+										onChange={e => this.fileUploaded(e.target.files[0])}
 									/>
 								);
+								inputInfo = (
+									<img
+										src={this.state.personalImage}
+										alt="Foto"
+										onClick={this.onPhotoClick}
+										onDragEnter={e => {
+											e.stopPropagation();
+											e.preventDefault();
+										}}
+										onDragOver={e => {
+											e.stopPropagation();
+											e.preventDefault();
+										}}
+										onDrop={e => {
+											e.stopPropagation();
+											e.preventDefault();
+											const fileObj = e.dataTransfer.files[0];
+											this.fileUploaded(fileObj);
+										}}
+									/>
+								);
+							} else if (editableFields === "true") {
+								inputOrDiv = <input type="text" defaultValue={item.field} />;
 							} else if (editableFields === "false") {
-								inputOrDiv = <div>{item.nombre}</div>;
+								notEditableClass = `notEditable${index}`;
+								inputOrDiv = <div>{item.field}</div>;
+							}
+							let fromToInputs = null;
+							if (fromToFields === "true") {
+								fromToInputs = (
+									<div className="fromTo">
+										<label htmlFor="from">Desde</label>
+										<input type="text" id="from" defaultValue={item.from} />
+										<label htmlFor="to">Hasta</label>
+										<input type="text" id="to" defaultValue={item.to} />
+									</div>
+								);
 							}
 							return (
-								<div key={item.id}>
+								<div key={item.id} className={notEditableClass}>
 									{inputOrDiv}
-									<input
-										type="text"
-										defaultValue={item.info}
-									/>
+									{inputInfo}
+									{fromToInputs}
 								</div>
 							);
 						})}
+
 						{addItemButton}
-						<button type="submit">Guardar cambios</button>
+						<button type="submit">
+							<FontAwesomeIcon icon={faCheck} className="icon" />
+							<div className="pipe">&nbsp;</div>
+							<div>Guardar</div>
+						</button>
 					</form>
 				</div>
 			);
@@ -109,23 +192,40 @@ class Section extends Component {
 			sectionRender = (
 				<div>
 					{sectionState.map(item => {
+						let divOrIcon;
+						if (icons === "null" || !icons.hasOwnProperty(item.field)) {
+							divOrIcon = <div>{item.field}</div>;
+						} else if (icons !== "null" && icons.hasOwnProperty(item.field)) {
+							divOrIcon = (
+								<div className="field">
+									<FontAwesomeIcon icon={icons[item.field]} />
+								</div>
+							);
+						}
 						return (
 							<div key={item.id} className="item">
-								<div className="nombre">{item.nombre}</div>
+								{divOrIcon}
 								<div className="info">{item.info}</div>
+								<div>{item.from}</div>
+								<div>{item.to}</div>
 							</div>
 						);
 					})}
-					<button onClick={this.edit}>Editar</button>
+					<button onClick={this.edit}>
+						<FontAwesomeIcon icon={faPenToSquare} className="icon" />
+						<div>Editar</div>
+					</button>
 				</div>
 			);
 		}
 
 		return (
-			<section>
-				<h2>{heading}</h2>
-				{singularOtherInfo}
-				{sectionRender}
+			<section className={stateName}>
+				<div className="sectionDiv">
+					<h2>{heading}</h2>
+					{singularOtherInfo}
+					{sectionRender}
+				</div>
 			</section>
 		);
 	}
